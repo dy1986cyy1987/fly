4680cba63ffb20745b80b4e94d65d2ea1e301fad
丁勇 <yongding@anjuke.com>
2015-12-12 22:44:49 +0800

    mac oxs

--- sys/autoload/functions.php
+++ sys/autoload/functions.php
@@ -1,25 +1,27 @@
 <?php
 
 /**
- * autoload action
  * @param $className
+ *
+ * @throws \fly\fly\SysException
  */
 function autoload($className)
 {
-    set_include_path(get_include_path() . PATH_SEPARATOR . SYS_PATH . 'fly');
-    
+    set_include_path(get_include_path() . PATH_SEPARATOR . SYS_PATH . 'fly' . DS . 'SysException.php');
+
     if (empty($className)) {
         throw new \fly\fly\SysException(json_encode(array(
             'status' => '100001',
             'errorMsg' => 'Class name is null!'
         )));
-        exit();
     }
-    
+
+    $is_linux_os = DS == '/';
+
     // namespace process
-    $php_file_prifix = str_replace('\\', DS, $className);
+    $php_file_prefix = str_replace('\\', DS, $className);
     
-    $php_file = $php_file_prifix . '.php';
+    $php_file = $php_file_prefix . '.php';
     
     trim($php_file, DS);
     
@@ -36,21 +38,21 @@ function autoload($className)
         $php_file = implode(DS, $arr_exp);
     } else {
         throw new \fly\fly\SysException(json_encode(array(
-            'status' => 10003,
+            'status' => 10002,
             'errorMsg' => 'Don\'t support the your private namespace! Only support namespace (' . NAMESPACE_APP_NAME . ', ' . NAMESPACE_SYS_NAME . ')'
         )));
-        exit();
     }
-    
+
+    $php_file = $is_linux_os ? DS . $php_file : $php_file;
+
     if (! file_exists($php_file)) {
         throw new \fly\fly\SysException(json_encode(array(
-            'status' => 10002,
+            'status' => 10003,
             'errorMsg' => 'File not exist!'
         )));
-        exit();
     }
-    
-    include_once ($php_file);
+
+    include_once($php_file);
 }
 
 spl_autoload_register('autoload');
