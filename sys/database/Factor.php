41c9ac5380efef8f581af0143dafbcea01a0714c
丁勇 <yongding@anjuke.com>
2015-12-17 11:33:07 +0800

    database update

--- sys/database/Factor.php
+++ sys/database/Factor.php
@@ -12,12 +12,85 @@ use \fly\interfaces as f_interfaces;
 class Factor implements f_interfaces\DbFactorInterface
 {
 
+    protected $sqlType;
+    protected $host;
+    protected $port;
+
+    /**
+     * 确定需要分库
+     *
+     * @var bool
+     */
+    protected $needPartDb = true;
+
+    /**
+     * 确定需要分表
+     *
+     * @var bool
+     */
+    protected $needPartTab = true;
+
+    /**
+     * it must be set in function changeFactory($factor) if it is a partial db
+     *
+     * @var string
+     */
+    private $dbName;
+
+    /**
+     * it must be set in function changeFactory($factor) if it is a partial table
+     *
+     * @var string
+     */
+    private $tabName;
+
+    /**
+     * 分库分表因子实现
+     *
+     * @param $factor \fly\database\Factor
+     */
     public function changeFactory($factor)
-    {}
+    {
 
+    }
+
+    /**
+     * get database name
+     *
+     * @return string
+     */
     public function getDbName()
-    {}
+    {
+        return $this->dbName;
+    }
 
+    /**
+     * get table name
+     *
+     * @return string
+     */
     public function getTableName()
-    {}
+    {
+        return $this->tabName;
+    }
+
+    /**
+     * get database type
+     *
+     * @return mixed
+     */
+    public function getSqlType()
+    {
+        return $this->sqlType;
+    }
+
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    public function getPort()
+    {
+        return $this->port;
+    }
 }
--- sys/database/factory/DbFactory.php
+++ sys/database/factory/DbFactory.php
@@ -3,6 +3,7 @@ namespace fly\database\factory;
 
 use \fly\utils as f_utils;
 use \fly\fly as f_fly;
+use \fly\database as f_database;
 
 class DbFactory
 {
@@ -14,20 +15,34 @@ class DbFactory
      */
     private static $_connections = array();
 
-    public static function getInstance($db_config_key = '', $factor)
+    /**
+     * @param string               $db_config_key
+     * @param \fly\database\Factor $factor
+     *
+     * @return mixed
+     * @throws f_fly\SysException
+     */
+    public static function getInstance($db_config_key = '', $factor = null)
     {
         if (empty($db_config_key)) {
             throw new f_fly\SysException('Db config key must not be empty!');
         }
-        
-        if (! empty(self::$_connections) && isset(self::$_connections[$db_config_key]) && ! empty(self::$_connections[$db_config_key])) {
+
+        if (!empty(self::$_connections) && isset(self::$_connections[$db_config_key]) && !empty(self::$_connections[$db_config_key])) {
             return self::$_connections[$db_config_key];
         }
-        
+
         $config = f_utils\DbTools::getDbConfig($db_config_key);
-        
+
         if ($config === false) {
             throw new f_fly\SysException('Db configure is wrong! key = ' . $db_config_key);
         }
+
+        if (empty($factor) || !$factor instanceof f_database\Factor) {
+
+        } else {
+
+        }
+
     }
 }
