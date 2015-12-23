fa2ea3d588f2763c3b4bb271587c32b38cab5a7c
丁勇 <yongding@anjuke.com>
2015-12-23 10:20:21 +0800

    push

--- sys/Fly.php
+++ sys/Fly.php
@@ -69,27 +69,27 @@ class Fly
      */
     private function init()
     {
-        if (! defined('__DEBUG')) {
+        if (!defined('__DEBUG')) {
             define('__DEBUG', true);
         }
-        
-        if (! defined('DS')) {
+
+        if (!defined('DS')) {
             define('DS', DIRECTORY_SEPARATOR);
         }
-        
-        if (! defined('SYS_PATH')) {
+
+        if (!defined('SYS_PATH')) {
             define('SYS_PATH', dirname(__FILE__));
         }
-        
-        if (! defined('NAMESPACE_APP_NAME')) {
+
+        if (!defined('NAMESPACE_APP_NAME')) {
             throw new \fly\fly\SysException('Please define NAMESPACE_APP_NAME first in the index.php');
         }
-        
-        if (! defined('NAMESPACE_SYS_NAME')) {
+
+        if (!defined('NAMESPACE_SYS_NAME')) {
             define('NAMESPACE_SYS_NAME', 'fly');
         }
-        
-        include_once (SYS_PATH . 'autoload' . DS . 'functions.php');
+
+        include_once(SYS_PATH . 'autoload' . DS . 'functions.php');
     }
 
     /**
@@ -98,10 +98,10 @@ class Fly
      */
     public static function &getInstance()
     {
-        if (! self::$instance) {
+        if (!self::$instance) {
             self::$instance = new self();
         }
-        
+
         return self::$instance;
     }
 
@@ -140,7 +140,7 @@ class Fly
     /**
      * set router
      *
-     * @param \fly\fly\Router $router            
+     * @param \fly\fly\Router $router
      */
     public function setRouter($router)
     {
@@ -152,24 +152,30 @@ class Fly
         return $this->router;
     }
 
+    /**
+     * The app entrance
+     *
+     * @return bool
+     * @throws \fly\fly\SysException
+     */
     public function run()
     {
         $this->beforeRun();
-        
+
         // execute before interceptors
         $interceptors = $this->getInterceptors();
-        
+
         $bol_exit = false;
-        
+
         if ($interceptors) {
             $interceptors = is_array($interceptors) ? $interceptors : array(
-                $interceptors
+                $interceptors,
             );
-            
+
             foreach ($interceptors as $interceptor) {
                 $obj_interceptor = new $interceptor();
                 $before_interceptor_result = $obj_interceptor->before();
-                
+
                 if ($before_interceptor_result == \fly\interceptors\SysGlobalInterceptor::STEP_BREAK) {
                     continue;
                 } elseif ($before_interceptor_result == \fly\interceptors\SysGlobalInterceptor::STEP_EXIT) {
@@ -178,37 +184,37 @@ class Fly
                 }
             }
         }
-        
+
         if ($bol_exit) {
             exit(0);
         }
-        
+
         // execute controller without render
         $controller_name = $this->getControllerName();
-        
+
         if (empty($controller_name)) {
             throw new \fly\fly\SysException('Can\'t find the controller!');
         }
-        
+
         $this->controller = new $controller_name();
-        
-        if (! $this->controller->checkParams()) {
+
+        if (!$this->controller->checkParams()) {
             throw new \fly\fly\SysException('Controller check params failed!');
         }
-        
+
         $this->controller->beforeHandle();
         $view = $this->controller->handle();
-        
+
         // execute after interceptor
         if ($interceptors) {
             $interceptors = is_array($interceptors) ? $interceptors : array(
-                $interceptors
+                $interceptors,
             );
-            
+
             foreach ($interceptors as $interceptor) {
                 $obj_interceptor = new $interceptor();
                 $before_interceptor_result = $obj_interceptor->after();
-                
+
                 if ($before_interceptor_result == \fly\interceptors\SysGlobalInterceptor::STEP_BREAK) {
                     continue;
                 } elseif ($before_interceptor_result == \fly\interceptors\SysGlobalInterceptor::STEP_EXIT) {
@@ -217,19 +223,19 @@ class Fly
                 }
             }
         }
-        
+
         if ($bol_exit) {
             exit(0);
         }
-        
+
         // render page
-        if (! $view) {
+        if (!$view) {
             return false;
         }
-        
+
         $this->view = new $view();
         $this->view->render();
-        
+
         return true;
     }
 
@@ -238,11 +244,11 @@ class Fly
         if (empty($this->router)) {
             $this->router = \fly\fly\Router::getInstance();
         }
-        
+
         if (empty($this->request)) {
             $this->request = \fly\fly\Request::getInstance();
         }
-        
+
         if (empty($this->response)) {
             $this->response = \fly\fly\Response::getInstance();
         }
@@ -282,7 +288,7 @@ class Fly
 
     public function setAttributes($attributes = array())
     {
-        if (is_array($attributes) && ! empty($attributes)) {
+        if (is_array($attributes) && !empty($attributes)) {
             $this->_attributes = array_merge($this->_attributes, $attributes);
         }
     }
@@ -292,7 +298,7 @@ class Fly
         if (array_key_exists($key, $this->_attributes)) {
             return $this->_attributes[$key];
         }
-        
+
         return false;
     }
 
--- sys/autoload/functions.php
+++ sys/autoload/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+set_include_path(get_include_path() . PATH_SEPARATOR . SYS_PATH . 'fly' . DS . 'SysException.php');
+
 /**
  * @param $className
  *
@@ -7,7 +9,6 @@
  */
 function autoload($className)
 {
-    set_include_path(get_include_path() . PATH_SEPARATOR . SYS_PATH . 'fly' . DS . 'SysException.php');
 
     if (empty($className)) {
         throw new \fly\fly\SysException(json_encode(array(
